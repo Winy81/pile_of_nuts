e12dd80f7a76242f85a05e7f06b61cf42b6c6af0
Winy81 <Adam.Nemeth@FootballPools.com>
2023-07-26 12:18:04 +0100

    Code upgrade

--- app/controllers/nuts_controller.rb
+++ app/controllers/nuts_controller.rb
@@ -16,7 +16,7 @@ class NutsController < ApplicationController
     @consumed_per_km = params[:consumed_per_km].to_f
     @cart_capacity = params[:cart_capacity].to_f
 
-    @max_transportable_nuts = NutsTransporterCalculator.compute_maximum_nuts(@distance, @nuts, @consumed_per_km, @cart_capacity)
+    @max_transportable_nuts = NutsTransporterCalculator.new(@distance, @nuts, @consumed_per_km, @cart_capacity).compute_maximum_nuts
   end
 
   def file_read
@@ -35,7 +35,7 @@ class NutsController < ApplicationController
         params = line.chomp.split(',').map(&:to_f)
         distance, nuts, consumed_per_km, cart_capacity = params
 
-        max_nuts_transportable = NutsTransporterCalculator.compute_maximum_nuts(distance, nuts, consumed_per_km, cart_capacity)
+        max_nuts_transportable = NutsTransporterCalculator.new(distance, nuts, consumed_per_km, cart_capacity).compute_maximum_nuts
         row_result = {:incoming_data => [distance, nuts, consumed_per_km, cart_capacity], :result => max_nuts_transportable}
         result << row_result
       end
--- app/services/nuts_transporter_calculator.rb
+++ app/services/nuts_transporter_calculator.rb
@@ -1,26 +1,61 @@
 class NutsTransporterCalculator
 
-  def self.compute_maximum_nuts(distance, nuts, consumed_per_km, cart_capacity)
+  def initialize(distance, nuts, consumed_per_km, cart_capacity)
 
-    remaining_nuts = nuts
-    delivered_nuts = 0
+    @remaining_nuts = nuts
+    @delivered_nuts = 0
 
-    single_trip = distance
-    return_trip = distance * 2
+    @single_trip = distance
+    @return_trip = distance * 2
+
+    @consumed_per_km = consumed_per_km
+    @cart_capacity = cart_capacity
+
+  end
+
+  def compute_maximum_nuts
 
     # While nuts can cover the trip there and back and also the cart has enough capacity for it
-    while (remaining_nuts - (return_trip * consumed_per_km)) > 0 && (return_trip * consumed_per_km) < cart_capacity
-      delivered_nuts += cart_capacity - (return_trip * consumed_per_km)
-      remaining_nuts = remaining_nuts - (return_trip * consumed_per_km) - delivered_nuts
+    while is_the_remaining_nuts_can_cover_on_both_ways? && is_the_capacity_enough_to_cover_both_ways?
+      @delivered_nuts += @cart_capacity - ate_supplement_on_there_and_back
+      @remaining_nuts = @remaining_nuts - ate_supplement_on_there_and_back - @delivered_nuts
     end
 
     # The last trip (or only trip if the return not possible) - can be - is a single if nuts can cover the trip only there and also the cart has enough capacity for it
-    if (remaining_nuts - (single_trip * consumed_per_km)) > 0 && (single_trip * consumed_per_km) < cart_capacity
-      last_trip_nuts = cart_capacity < remaining_nuts ? cart_capacity - (single_trip * consumed_per_km) : remaining_nuts - (single_trip * consumed_per_km)
-      delivered_nuts += last_trip_nuts
+    if  is_the_remaining_nuts_can_cover_a_full_way? && is_the_capacity_enough_to_cover_a_full_way?
+      @last_trip_nuts = @cart_capacity < @remaining_nuts ? @cart_capacity - ate_supplement_on_single_trip : @remaining_nuts - ate_supplement_on_single_trip
+      @delivered_nuts += @last_trip_nuts
     end
 
-    @delivered_nuts = delivered_nuts
+    @delivered_nuts
 
   end
+
+  private
+
+  def is_the_remaining_nuts_can_cover_on_both_ways?
+    (@remaining_nuts - (@return_trip * @consumed_per_km)) > 0
+  end
+
+  def is_the_remaining_nuts_can_cover_a_full_way?
+    (@remaining_nuts - (@single_trip * @consumed_per_km)) > 0
+  end
+
+  def is_the_capacity_enough_to_cover_both_ways?
+    (@return_trip * @consumed_per_km) < @cart_capacity
+  end
+
+  def is_the_capacity_enough_to_cover_a_full_way?
+    (@single_trip * @consumed_per_km) < @cart_capacity
+  end
+
+  def ate_supplement_on_there_and_back
+    @return_trip * @consumed_per_km
+  end
+
+  def ate_supplement_on_single_trip
+    @single_trip * @consumed_per_km
+  end
+
 end
+

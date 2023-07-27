f5eb46915b04e396be05b0e48ab31d48ed27b5d2
Winy81 <Adam.Nemeth@FootballPools.com>
2023-07-27 09:20:55 +0100

    Code optimalization

--- app/controllers/nuts_controller.rb
+++ app/controllers/nuts_controller.rb
@@ -11,12 +11,14 @@ class NutsController < ApplicationController
   end
 
   def compute_manual_read
+
     @distance = params[:distance].to_f
     @nuts = params[:nuts].to_f
     @consumed_per_km = params[:consumed_per_km].to_f
     @cart_capacity = params[:cart_capacity].to_f
 
-    @max_transportable_nuts = NutsTransporterCalculator.new(@distance, @nuts, @consumed_per_km, @cart_capacity).compute_maximum_nuts
+    @result = max_deliverable_nuts_calculation(@distance, @nuts, @consumed_per_km, @cart_capacity)
+
   end
 
   def file_read
@@ -35,14 +37,20 @@ class NutsController < ApplicationController
         params = line.chomp.split(',').map(&:to_f)
         distance, nuts, consumed_per_km, cart_capacity = params
 
-        max_nuts_transportable = NutsTransporterCalculator.new(distance, nuts, consumed_per_km, cart_capacity).compute_maximum_nuts
+        max_nuts_transportable = max_deliverable_nuts_calculation(distance, nuts, consumed_per_km, cart_capacity)
         row_result = {:incoming_data => [distance, nuts, consumed_per_km, cart_capacity], :result => max_nuts_transportable}
         result << row_result
       end
 
       @result = result
-
+      
     end
-
   end
+
+  private
+
+  def max_deliverable_nuts_calculation(distance, nuts, consumed_per_km, cart_capacity)
+    NutsTransporterCalculator.new(distance, nuts, consumed_per_km, cart_capacity).compute_maximum_nuts
+  end
+
 end
